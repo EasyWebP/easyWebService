7e4012c07ddfb8800ec6dd4f7c7b76c98c2ac2d7
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-15 14:41:39 +0900

    first commit

---
+++ src/main/java/easyweb/easywebservice/EasyWebServiceApplication.java
@@ -0,0 +1,13 @@
+package easyweb.easywebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/easyweb/easywebservice/EasyWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package easyweb.easywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
