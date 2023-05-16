2c821f5780cffbf676c960ff4ef37d8a4d01148c
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-16 15:44:21 +0900

    [ADD] Member Nickname update

--- src/main/java/easyweb/easywebservice/domain/Member/model/Member.java
+++ src/main/java/easyweb/easywebservice/domain/Member/model/Member.java
@@ -14,11 +14,12 @@ import java.util.List;
 public class Member extends MemberBase {
     private Boolean deleted;
     private String userName;
-
+    private String nickName;
     @Builder
-    public Member(Long id, String email, String password, Authority authority, UserLoginType userLoginType, String userName, Boolean deleted) {
+    public Member(Long id, String email, String password, Authority authority, UserLoginType userLoginType, String userName, String nickName, Boolean deleted) {
         super(id, email, password, authority, userLoginType);
         this.userName = userName;
+        this.nickName = nickName;
         this.deleted = deleted;
     }
 
@@ -36,4 +37,7 @@ public class Member extends MemberBase {
     public void updateUserName(String userName) {
         this.userName = userName;
     }
+
+    public void updateNickName(String nickName) {
+        this.nickName = nickName;}
 }
